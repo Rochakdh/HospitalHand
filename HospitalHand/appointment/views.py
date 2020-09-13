d06632d912a9e6cf184147d2b7e15bf1bed4f8c7
Prerit Bhandari <patrioticprerit@gmail.com>
2020-09-13 16:52:20 +0545

    [Made] Some changes to errors at serializers

--- HospitalHand/appointment/views.py
+++ HospitalHand/appointment/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, UpdateAPIView, ListAPIView, RetrieveUpdateAPIView, DestroyAPIView,RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, UpdateAPIView, ListAPIView, RetrieveUpdateAPIView, \
+    DestroyAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 from hospital.models import Hospital
 from userauth.models import CustomUser
-from .serializers import AppointmentSerializers, AppointmentFixSerializers, AppointmentUpdateSerializers,DoctorAppointmentSerializers
+from .serializers import AppointmentSerializers, AppointmentFixSerializers, AppointmentUpdateSerializers, \
+    DoctorAppointmentSerializers
 from .models import Appointment
-from .permissions import HospitalIsAuthenticated,HospitalIsObjectAuthenticated
+from .permissions import HospitalIsAuthenticated, HospitalIsObjectAuthenticated
 
 
 # Create your views here.
@@ -54,12 +56,13 @@ class DeleteAppointment(DestroyAPIView):
     queryset = Appointment.objects.all()
     lookup_field = 'id'
 
+
 class DoctorAppointment(ListAPIView):
     serializer_class = DoctorAppointmentSerializers
     queryset = Appointment.objects.all()
     lookup_field = 'doctor_requested'
     lookup_url_kwarg = 'doctor_requested'
-    permission_classes = [HospitalIsAuthenticated,HospitalIsObjectAuthenticated]
+    permission_classes = [HospitalIsAuthenticated, HospitalIsObjectAuthenticated]
 
     def get_queryset(self):
         """
@@ -69,4 +72,4 @@ class DoctorAppointment(ListAPIView):
         # user = self.request.user
         # return Appointment.objects.filter(selected_doctor=self.request.)
         super(DoctorAppointment, self).get_queryset()
-        return self.queryset.filter(doctor_requested_id = self.kwargs['doctor_requested_id'])
+        return self.queryset.filter(doctor_requested_id=self.kwargs['doctor_requested_id'])
